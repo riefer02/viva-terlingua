40886bdb34dffa1dfcf5daf296be018d300415ed
riefer02 <andrew.riefenstahl@gmail.com>
2021-06-15 08:46:13 -0500

    update QuickNav component

--- src/components/QuickNav.js
+++ src/components/QuickNav.js
@@ -1,5 +1,18 @@
 import React from "react";
+import { Link } from "gatsby";
 
 export default function QuickNav() {
-  return <h1>Quick Nav</h1>;
+  return (
+    <div>
+      <h1>Quick Nav</h1>
+      <ul>
+        <Link to="/">Home</Link>
+        <Link to="/tickets">Tickets</Link>
+        <Link to="/events">Events</Link>
+        <Link to="/music">Music</Link>
+        <Link to="/local-attractions">Local Attractions</Link>
+        <Link to="/about">About</Link>
+      </ul>
+    </div>
+  );
 }
