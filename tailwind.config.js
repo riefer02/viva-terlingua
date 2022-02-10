f7b1d60c55eb96ae8b84f1c5136a59f3b3cfdf65
riefer02 <andrew.riefenstahl@gmail.com>
2022-02-09 21:00:49 -0600

    updated packages

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 module.exports = {
-  purge: ["./src/**/*.{js,jsx,ts,tsx}"],
-  darkMode: false, // or 'media' or 'class'
+  purge: ['./src/**/*.{js,jsx,ts,tsx}'],
+  media: false, // or 'media' or 'class'
   theme: {
     extend: {},
   },
