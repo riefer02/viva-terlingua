97cb2f1bd0f54aaa9d5dbd264a12e4fd090754bb
Andrew Riefenstahl <andrew.riefenstahl@gmail.com>
2022-07-10 15:41:01 -0500

    404 page humor

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,11 +1,13 @@
-import React from "react";
-import Layout from "components/Layout";
+import React from 'react';
+import Layout from 'components/Layout';
 
 const NotFoundPage = () => {
   return (
     <Layout>
       <h1>Page Not Found</h1>
-      <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+      <p>
+        You just hit a route that doesn&#39;t exist... let's get some chili!
+      </p>
     </Layout>
   );
 };
