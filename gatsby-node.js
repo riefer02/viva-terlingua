2c76631b2f9a4a99d1af5c5ce0cce2d1d17c5bee
Andrew Riefenstahl <andrew.riefenstahl@gmail.com>
2022-07-30 17:40:25 -0500

    build dependencies for functions on post build

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,5 +1,22 @@
 const { data } = require('autoprefixer');
 const path = require(`path`);
+const util = require('util');
+const child_process = require('child_process');
+const exec = util.promisify(child_process.exec);
+
+exports.onPostBuild = async (gatsbyNodeHelpers) => {
+  const { reporter } = gatsbyNodeHelpers;
+
+  const reportOut = (report) => {
+    const { stderr, stdout } = report;
+    if (stderr) reporter.error(stderr);
+    if (stdout) reporter.info(stdout);
+  };
+
+  // NOTE: the gatsby build process automatically copies /static/functions to /public/functions
+  // If you use yarn, replace "npm install" with "yarn install"
+  reportOut(await exec('cd ./public/functions && npm install'));
+};
 
 exports.createPages = async ({ actions, graphql }) => {
   const { data } = await graphql(`
---
+++ src/static/functions/record-transaction.js
@@ -0,0 +1,69 @@
+const stripe = require('stripe')(process.env.STRIPE_API_KEY);
+const webhookSecret = process.env.STRIPE_WEBHOOK_SECRET;
+const axios = require('axios');
+
+const recordTransaction = async (session) => {
+  const personalInfo = session.client_reference_id.split('—');
+  const fullName = `${personalInfo[0]} ${personalInfo[1]}`;
+  const date = new Date();
+
+  const transaction = {
+    name: fullName,
+    firstName: personalInfo[0],
+    lastName: personalInfo[1],
+    numberOfTickets: personalInfo[2],
+    email: session.customer_details.email,
+    phoneNumber: personalInfo[3],
+    customerID: session.customer,
+    transactionID: session.id,
+    timeOfPurchase: personalInfo[4],
+    recipientFirstName: personalInfo[5] ? personalInfo[5] : '',
+    recipientLastName: personalInfo[6] ? personalInfo[6] : '',
+    cookOffYear: date.getFullYear(),
+  };
+
+  await axios
+    .post(`${process.env.STRAPI_URL}/ticket-holders`, transaction)
+    .then((res) => {
+      return {
+        statusCode: 200,
+        body: { message: 'success', data: res.data },
+      };
+    })
+    .catch((err) => {
+      return {
+        statusCode: 400,
+        body: { message: err.message },
+      };
+    });
+};
+
+exports.handler = async function ({ body, headers }, context) {
+  const payload = body;
+  const sig = headers['stripe-signature'];
+
+  try {
+    const stripeEvent = await stripe.webhooks.constructEvent(
+      payload,
+      sig,
+      webhookSecret
+    );
+
+    if (stripeEvent.type === 'checkout.session.completed') {
+      const session = stripeEvent.data.object;
+      await recordTransaction(session);
+    }
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify({
+        received: true,
+      }),
+    };
+  } catch (err) {
+    return {
+      statusCode: 400,
+      body: `Webhook Error: ${err.message}`,
+    };
+  }
+};
