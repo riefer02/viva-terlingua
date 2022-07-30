6b50da16227679ec7d8f73226c51f26fc02e4d42
Andrew Riefenstahl <andrew.riefenstahl@gmail.com>
2022-07-30 17:57:55 -0500

    debug functions part 3

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,21 +1,15 @@
 const { data } = require('autoprefixer');
 const path = require(`path`);
-const util = require('util');
-const child_process = require('child_process');
-const exec = util.promisify(child_process.exec);
+const fs = require('fs');
 
-exports.onPostBuild = async (gatsbyNodeHelpers) => {
-  const { reporter } = gatsbyNodeHelpers;
-
-  const reportOut = (report) => {
-    const { stderr, stdout } = report;
-    if (stderr) reporter.error(stderr);
-    if (stdout) reporter.info(stdout);
-  };
-
-  // NOTE: the gatsby build process automatically copies /static/functions to /public/functions
-  // If you use yarn, replace "npm install" with "yarn install"
-  reportOut(await exec('cd public/functions && npm install'));
+const { zipFunctions } = require('@netlify/zip-it-and-ship-it');
+exports.onPostBuild = () => {
+  const srcLocation = path.join(__dirname, `./functions`);
+  const outputLocation = path.join(__dirname, `./public/functions`);
+  if (!fs.existsSync(outputLocation)) {
+    fs.mkdirSync(outputLocation);
+  }
+  return zipFunctions(srcLocation, outputLocation);
 };
 
 exports.createPages = async ({ actions, graphql }) => {
