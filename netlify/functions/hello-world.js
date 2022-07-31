ebb95e6b338988238544a56d16b380887df57d8b
Andrew Riefenstahl <andrew.riefenstahl@gmail.com>
2022-07-31 08:54:46 -0500

    continued testing

--- netlify/functions/hello-world.js
+++ netlify/functions/hello-world.js
@@ -2,9 +2,12 @@ exports.handler = async function (event, context) {
   // your server-side functionality
   return {
     statusCode: 200,
+    headers: {
+      Test: 'Test',
+    },
     body: JSON.stringify({
-      message: 'Hello World',
-      envTest: process.env.NODE_ENV,
+      gibberish: 'Gibberish',
+      envTest: `${process.env.NODE_ENV} hey~!`,
     }),
   };
 };
