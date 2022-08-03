c05502025b9218ddb260760b0291281e0a30004d
Andrew Riefenstahl <andrew.riefenstahl@gmail.com>
2022-08-03 10:37:24 -0500

    fix seo index page title

--- src/pages/index.js
+++ src/pages/index.js
@@ -29,7 +29,7 @@ const IndexPage = ({ data }) => {
   return (
     <Layout>
       <Seo
-        title={title}
+        title={"Home"}
         description={meta.description}
         keywords={[
           `terlingua`,
