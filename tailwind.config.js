d6f88726fb77e48a2d1e06d489a72517c4fa984b
Andrew Riefenstahl <andrew.riefenstahl@gmail.com>
2022-08-06 15:42:24 -0500

    resolve misconfigured tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,11 @@ module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   media: false, // or 'media' or 'class'
   theme: {
-    colors: {
-      primary: '#e74853',
+    extend: {
+      colors: {
+        primary: '#e74853',
+      },
     },
-    extend: {},
   },
   variants: {
     extend: {},
