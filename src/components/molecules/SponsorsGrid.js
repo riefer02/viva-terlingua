818257afef2b0eac4ed826b48ec3e0f9ff002162
Andrew Riefenstahl <andrew.riefenstahl@gmail.com>
2022-08-26 07:59:56 -0500

    adjust max-width

--- src/components/molecules/SponsorsGrid.js
+++ src/components/molecules/SponsorsGrid.js
@@ -9,10 +9,10 @@ export default function SponsorsGrid({ sponsorLogos } ) {
           We Love Our Sponsors
         </h3>
       </div>
-      <div className="text-white border border-tertiary-light bg-white p-8 shadow-lg max-w-[880px] mx-auto">
+      <div className="text-white border border-tertiary-light bg-white p-8 shadow-lg mx-auto">
         <div className="flex flex-wrap items-center gap-10 justify-between p-8 bg-primary-light shadow-md">
           {sponsorLogos.map((image, index) => (
-            <div className="w-full md:w-1/4 h-[200px] overflow-hidden flex items-center justify-center p-4">
+            <div className="w-full md:w-1/4 lg:w-[15%] h-[200px] overflow-hidden flex items-center justify-center p-4">
               <GatsbyImage
                 placeholder="blurred"
                 formats="[AVIF,WEBP]"
