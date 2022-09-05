601fd90682c5befc5d94c0fef70ddd440335f1f1
Andrew Riefenstahl <andrew.riefenstahl@gmail.com>
2022-09-05 10:24:16 -0500

    update contact info

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,12 +4,13 @@ import Container from "components/Container";
 
 const Footer = () => {
   return (
-    <footer className="footer">
+    <footer className="footer py-8 h-full mt-3">
       <Container>
-        <div className="footer__content">
-          <div className="footer__copyright">
+        <div className="footer__content gap-2 md:gap-6 lg:gap-10 items-center h-full">
+          <div className="footer__copyright text-2xl lg:text-3xl text-secondary">
             &copy; {new Date().getFullYear()}, Viva Terlingua!
           </div>
+          <div className="text-2xl lg:text-3xl">Contact Us: <span className="text-primary">abowlofred@gmail.com</span></div>
         </div>
       </Container>
     </footer>
