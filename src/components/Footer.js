da9c2a507d65a2fd7d3fe4f68e7aa91fb2bb7415
Andrew Riefenstahl <andrew.riefenstahl@gmail.com>
2022-10-02 14:39:59 -0500

    add rief productions to footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,16 +1,21 @@
-import React from "react";
+import React from 'react';
 
-import Container from "components/Container";
+import Container from 'components/Container';
 
 const Footer = () => {
   return (
     <footer className="footer py-8 mt-3">
       <Container>
-        <div className="footer__content gap-2 md:gap-6 lg:gap-10 items-center h-full">
-          <div className="footer__copyright text-2xl lg:text-3xl text-gray-700">
-            &copy; {new Date().getFullYear()}, Viva Terlingua!
+        <div className="flex flex-col items-center gap-2 h-full">
+          <div className="flex gap-8">
+            <div className="text-2xl text-gray-700">
+              &copy; {new Date().getFullYear()}, Viva Terlingua! | Contact Us:{' '}
+              <span className="text-primary">abowlofred@gmail.com</span>
+            </div>
           </div>
-          <div className="text-2xl lg:text-3xl text-gray-700">Contact Us: <span className="text-primary">abowlofred@gmail.com</span></div>
+          <div className="text-gray-700 text-lg">
+            Website by Rief Productions LLC
+          </div>
         </div>
       </Container>
     </footer>
