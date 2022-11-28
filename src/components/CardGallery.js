bf014128ac9c995021a560ecb230c76055e7380f
Andrew Riefenstahl <andrew.riefenstahl@gmail.com>
2022-11-27 18:34:12 -0600

    update text size of cards

--- src/components/CardGallery.js
+++ src/components/CardGallery.js
@@ -8,12 +8,12 @@ function Card({ item }) {
   // `slug` for internal links, `url` for external links
   const { image, slug, name, url, squareImage } = item.node;
   const cardImage = squareImage ? squareImage : image;
-  const cardStyles = `p-4 block group`;
+  const cardStyles = `p-4 block group max-w-[400px] mx-auto`;
   const cardContentStyles = 'shadow-md ';
   const cardLinkImgWrapperStyles = `block`;
   const cardGalleryCardImgStyles = 'rounded-t-lg h-[300px] ';
   const cardTextAreaStyles = `bg-primary-light group-hover:bg-secondary transition ease-linear text-white rounded-b-lg`;
-  const cardGalleryCardTitleStyles = `text-3xl my-4 lg:my-2 inline-block`;
+  const cardGalleryCardTitleStyles = `text-4xl lg:text-2xl my-4 inline-block px-2`;
 
   const cardContent = () => (
     <article className={cardContentStyles}>
@@ -26,9 +26,9 @@ function Card({ item }) {
       <div className={cardTextAreaStyles}>
         <h2 className={cardGalleryCardTitleStyles}>{name}</h2>
         <FontAwesomeIcon
-          size="lg"
+          size=""
           icon="arrow-right"
-          className="ml-3 relative bottom-1"
+          className="ml-3 relative bottom-1 h-6 lg:h-4"
         ></FontAwesomeIcon>
       </div>
     </article>
