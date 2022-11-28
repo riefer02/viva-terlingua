79d58de192513f5297b009efee76dc31666b7d87
Andrew Riefenstahl <andrew.riefenstahl@gmail.com>
2022-11-27 18:21:47 -0600

    refactor cards component

--- src/pages/index.js
+++ src/pages/index.js
@@ -58,7 +58,7 @@ const IndexPage = ({ data }) => {
             <div className="-skew-x-12 inline-block px-8 py-3 skew-x-10 shadow-md absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] z-20 text-5xl bg-primary-light group-hover:bg-secondary text-white transition ease-linear pointer-events-none">
               <div className="skew-x-12">Visit Store</div>
             </div>
-            <GatsbyImage image={getImage(cookoffStoreImg.image)} />
+            <GatsbyImage image={getImage(cookoffStoreImg.image)} alt="terlingua cook off store ad" />
           </div>
         </a>
         <OfficialCookOffPoster className="hidden lg:block" />
