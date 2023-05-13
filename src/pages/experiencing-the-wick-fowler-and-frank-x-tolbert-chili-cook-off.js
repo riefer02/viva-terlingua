f52abf023920a193de8f888aa1efc3150eb0ac2e
Andrew Riefenstahl <andrew.riefenstahl@gmail.com>
2023-05-13 10:05:17 -0500

    successfully build with new node version + update package.json-lock

---
+++ src/pages/experiencing-the-wick-fowler-and-frank-x-tolbert-chili-cook-off.js
@@ -0,0 +1,91 @@
+import React from 'react';
+import Layout from 'components/Layout';
+import Seo from 'components/SEO';
+
+export default function ExperiencingTheCookOffPage() {
+  return (
+    <div>
+      <Layout>
+        <Seo
+          title={
+            'Experiencing the Wick Fowler and Frank X. Tolbert Chili Cook Off'
+          }
+          description={`Discover the ultimate chili lover's paradise at the Wick Fowler and Frank X. Tolbert Chili Cook Off in Terlingua, Texas. Join us for a mouth-watering adventure!`}
+          keywords={[
+            `terlingua`,
+            `chili`,
+            `cook off`,
+            `tolbert`,
+            `wick fowler`,
+            `original`,
+            'international',
+            '2022',
+          ]}
+        />
+        <div className="bg-tertiary-light py-10 px-10 max-w-[982px] w-full mx-auto shadow-lg my-20 rounded-xl">
+          <div class=" py-10 px-4 md:px-10 bg-white shadow-lg rounded-xl">
+            <h1 class="text-4xl md:text-5xl font-bold !leading-[1.7] underline text-center mb-6 md:mb-10 max-w-6xl mx-auto">
+              Experiencing the Wick Fowler and Frank X. Tolbert Chili Cook Off
+            </h1>
+            <h2 class="text-2xl md:text-3xl font-bold mb-4">Introduction</h2>
+            <p class="text-xl md:text-2xl lg:text-3xl mb-6 !leading-[1.6]">
+              The Wick Fowler and Frank X. Tolbert Chili Cook Off in Terlingua,
+              Texas, is the ultimate destination for chili lovers from around
+              the world, drawing thousands of attendees every year.{' '}
+            </p>
+            <h2 class="text-2xl md:text-3xl font-bold mb-4">
+              The Friendly Atmosphere
+            </h2>
+            <p class="text-xl md:text-2xl lg:text-3xl mb-6 !leading-[1.6]">
+              The friendliness of the people is one of the first things that
+              struck me about the event. Everyone seemed genuinely happy to be
+              there and eager to share their love of chili. It was clear that
+              everyone was united by their passion for chili, making it a truly
+              inclusive and welcoming atmosphere.
+            </p>
+            <h2 class="text-2xl md:text-3xl font-bold mb-4">
+              The Unique Location
+            </h2>
+            <p class="text-xl md:text-2xl lg:text-3xl mb-6 !leading-[1.6]">
+              The location of the cookoff is also quite unique, nestled in the
+              Chisos Mining Valley. The stunning backdrop of the valley added to
+              the excitement and made the experience all the more memorable. As
+              a geology enthusiast, I was fascinated by the rocky terrain and
+              the history of the area.
+            </p>
+            <h2 class="text-2xl md:text-3xl font-bold mb-4">
+              Local Attractions
+            </h2>
+            <p class="text-xl md:text-2xl lg:text-3xl mb-6 !leading-[1.6]">
+              During my stay in Terlingua, I had the chance to explore some of
+              the local attractions, including Big Bend National Park and the
+              ghost town of Terlingua. The park was absolutely breathtaking,
+              with stunning vistas and rugged landscapes that left me in awe.
+              The ghost town was equally fascinating, with a rich history and an
+              eerie atmosphere that sent shivers down my spine.
+            </p>
+            <h2 class="text-2xl md:text-3xl font-bold mb-4">
+              The Taste and Smell of Chili
+            </h2>
+            <p class="text-xl md:text-2xl lg:text-3xl mb-6 !leading-[1.6]">
+              But of course, the highlight of my trip was the chili. The aroma
+              of the spices wafting through the air was enough to make my mouth
+              water. I sampled as many varieties as I could, each with its own
+              unique blend of ingredients and flavors. But the one that stood
+              out to me was a classic bowl of red chili. It was rich, hearty,
+              and bursting with flavor, leaving me feeling warm and satisfied.
+            </p>
+            <h2 class="text-2xl md:text-3xl font-bold mb-4">Conclusion</h2>
+            <p class="text-xl md:text-2xl lg:text-3xl mb-6 !leading-[1.6]">
+              Overall, the Wick Fowler and Frank X. Tolbert Chili Cook Off was
+              an unforgettable experience. From the friendly people and unique
+              location to the local attractions and delicious chili, everything
+              came together to create a truly magical event. I can't wait to go
+              back next year and experience it all over again.
+            </p>
+          </div>
+        </div>
+      </Layout>
+    </div>
+  );
+}
