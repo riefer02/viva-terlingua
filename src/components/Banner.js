673e346da393f1fd141c0cc93b962c3cdc7f670e
Andrew Riefenstahl <andrew.riefenstahl@gmail.com>
2024-02-17 13:31:07 -0600

    marquee I padding

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -9,11 +9,13 @@ export default function Banner() {
     <div className="banner hidden sm:flex items-center justify-center bg-gray-light-1 min-h-[50px]">
       <div className="flex items-center justify-center w-full container mx-auto ">
         <p className="text-lg">
-          Buy Tickets
+          {/* Buy Tickets
           <Link to="/tickets" className={linkStyle}>
             Here
           </Link>
-          for the {new Date().getFullYear()} Terlingua Chili Cook Off
+          for the {new Date().getFullYear()} Terlingua Chili Cook Off */}
+          Fun Fact: Many <span className="text-primary">chili peppers</span>{' '}
+          rely on bees for pollination.
         </p>
         {/* <p className="banner__message">
           <Link to="/resources" className="banner__message-link">
