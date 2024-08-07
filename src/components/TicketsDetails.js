a984fc9f4a5920befe6975849d18bd5f826fbbbb
Andrew Riefenstahl <andrew.riefenstahl@gmail.com>
2024-08-07 18:09:41 -0500

    add ticket price

--- src/components/TicketsDetails.js
+++ src/components/TicketsDetails.js
@@ -8,9 +8,12 @@ export default function TicketsDetails() {
     <article className="bg-tertiary-light p-4 lg:p-4 lg:rounded-lg">
       <section className="container mx-auto p-2 px-4 md:p-4 bg-gray-light-1 shadow-md rounded-lg">
         <div className="prose lg:prose-lg mx-auto">
-          <header>
+          <header className="-mb-6">
             <p>The 2024 Ticket portal is now open!</p>
             <p>Oct 30th - Nov 2nd, {CURRENT_YEAR}</p>
+            <p>
+              Price: <span className="text-primary">$53</span> per Chilihead!
+            </p>
           </header>
 
           <section>
